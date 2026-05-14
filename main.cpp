166d972da14126abfaba48c4f613ad2e2af6b353
AnirvinyaKolli <anirvinya.kolli.01@gmail.com>
2026-05-14 08:46:54 -0400

    Finsiehd

--- main.cpp
+++ main.cpp
@@ -3,25 +3,44 @@
 #include <string>
 using std::vector, std::string;
 
+// Car struct
+struct car{
+    string brand;
+    string model; 
+    string year; 
+};
+
+//Displaying the vector
+void displayVector(vector<car>& vector);
+
+
+
 int main(){
-    vector<string> pets {"dog", "cat", "fish"}; 
+    // Ford, Benz, Toyota, BMW, Subaru  
+    vector<car> cars {
+        {"Ford","Explorer","2026"},
+        {"Benz","C-Class","1993"},
+        {"Toyota","Avalon","2018"},
+        {"BMW","X1","2022"},
+        {"Subaru","Impreza","1994"}
+    };
     
-    for (auto itr = pets.begin(); itr != pets.end(); itr++)
-        std::cout << *itr << "\n";
-        
-    std::cout << "_____________________________________" << "\n"; 
-
-    pets.insert(pets.begin() + 2 , "bird"); 
-
-    for (string str : pets) {
-        std::cout << str << "\n";
-    }
+    displayVector(cars); 
     
-    std::cout << "_____________________________________" << "\n"; 
+    // Insert Volskswagon after Benz
+    cars.insert(cars.begin() + 2 , {"Volkswagon", "Polo", "2017"}); 
+    displayVector(cars); 
 
-    pets.erase(pets.begin() + 1); 
-
-    for (string str : pets) {
-        std::cout << str << "\n";
-    }
+    // Erase Toyota
+    cars.erase(cars.begin() + 3); 
+    displayVector(cars); 
 }   
+
+//Displaying the vector
+void displayVector(vector<car>& vect){
+    std::cout << "Cars: " "\n"; 
+    for (car c: vect){
+        std::cout << "   " << c.brand << " " << c.model << ", " << c.year << "\n";
+    }
+    std::cout << "_________________________" << "\n";
+}
